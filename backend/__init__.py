1a74c4d3ab10d2bb21d65c088c1fc410b3dc869c
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-01-23 01:10:04 +0300

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,7 @@
+# Создайте __init__.py в backend
+touch backend/__init__.py
+
+# И в подпапках
+touch backend/services/__init__.py
+touch backend/models/__init__.py
+touch backend/routes/__init__.py
