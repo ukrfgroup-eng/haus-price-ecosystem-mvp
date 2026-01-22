675cf856b4b107a80c7d4a2b637c8ed497fa42f2
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-01-23 01:23:16 +0300

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,1 @@
-# Создайте __init__.py в backend
-touch backend/__init__.py
-
-# И в подпапках
-touch backend/services/__init__.py
-touch backend/models/__init__.py
-touch backend/routes/__init__.py
+find backend -type d -exec touch {}/__init__.py \;
