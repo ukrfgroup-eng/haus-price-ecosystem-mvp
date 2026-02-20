38771c03eba7c6c44f7af2974b0723bab314c22f
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-02-20 20:34:00 +0300

    Create config.py

---
+++ BLOCK_A_PARTNERS_DB/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://admin:admin123@localhost:5432/haus_partners')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+config = Config()
+
+def load_config():
+    return config
+
+def get_database_url():
+    return config.SQLALCHEMY_DATABASE_URI
